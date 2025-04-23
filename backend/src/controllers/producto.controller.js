fb5cb5ebe9e68fecad19b6cdbfe35193892e4040
JossyBR <jossybr94@gmail.com>
2025-04-22 20:37:49 -0500

    Crud listo y funcional en postman

--- backend/src/controllers/producto.controller.js
+++ backend/src/controllers/producto.controller.js
@@ -63,7 +63,106 @@ const create = async (req, res) => {
 
 };
 
+const update = async (req, res) => {
+  try {
+
+    const idProducto = req.params.id;
+    const producto = await Producto.findByPk(idProducto);
+
+    if(!producto) {
+      return res.status(404).json({
+        msg: `Producto a actualizar con el id: ${idProducto}, no encontrado en base de datos.`,
+        data: null,
+      })
+    }
+
+    const productoUpdate = await Producto.update(req.body, {
+      where: { id: idProducto},
+    })
+
+    const productoUpdated =await Producto.findByPk(idProducto);
+
+    return res.status(200).json({
+      msg: `Producto con el id ${idProducto}, actualizado de forma correcta`,
+      data:productoUpdated,
+    })
+    
+  } catch (error) {
+    return res.status(500).json({ 
+      status: false, 
+      msg: `Error al actualizar un producto: ${error.message}`, 
+      data: null, 
+    }); 
+  }
+}
+
+const destroy = async (req, res) => {
+  try { 
+      const idProducto = req.params.id; 
+      console.log("ID:", idProducto);
+      const producto = await Producto.findByPk(idProducto); 
+      
+      if (!producto) { 
+        return res.status(404).json({ 
+          status: false, 
+          msg: `Producto a eliminar con el id: ${idProducto}, no encontrado en base de datos.`, 
+          data: null, 
+        }); 
+      } 
+      await  producto.destroy(); 
+
+      return res.status(200).json({ 
+        status: true, 
+        msg: `Producto con el id: ${idProducto}, eliminado de forma correcta`, 
+      }); 
+   
+    } catch (error) { 
+      return res.status(500).json({ 
+        status: false, 
+        msg: `Error al eliminar un producto: ${error.message}`,                                                         
+      }); 
+    } 
+};
+
+
+const show = async (req, res) => {
+  try { 
+      const id = req.params.id; 
+   
+      const producto = await Producto.findByPk(id); 
+      if (!producto) { 
+        return res.status(404).json({ 
+          status: false, 
+          msg: `Producto con el id: ${id}, no encontrado en base de datos.`, 
+          data: null, 
+        }); 
+      } 
+   
+      return res.status(200).json({ 
+        status: true, 
+        msg: "Producto encontrado de forma correcta", 
+        data: producto, 
+      }); 
+    } catch (error) { 
+      return res.status(500).json({ 
+        status: false, 
+        msg: `Error al consultar un usuario: ${error.message}`, 
+        data: null, 
+      }); 
+    } 
+
+
+  // try {
+  //     res.status(200).json({message: "Ingreso al controlador de show"})    
+  // } catch (error) {
+  //     res.status(400).json({message: "Ruta no encontrada"})
+  // }
+};
+
 module.exports = {  
   index,
   create,
+  update,
+  destroy,
+  show,
 };
--- backend/src/routes/producto.routes.js
+++ backend/src/routes/producto.routes.js
@@ -4,10 +4,14 @@ const router = express.Router();
 
 //Se importa el controlador
 const productoController = require("../controllers/producto.controller.js");
+const { update } = require("../models/producto.models.js");
 
 //Se crean las rutas
 
 router.get("/", productoController.index);
 router.post('/', productoController.create)
+router.put('/:id', productoController.update)
+router.delete('/:id', productoController.destroy)
+router.get('/:id', productoController.show)
 
 module.exports = router;
